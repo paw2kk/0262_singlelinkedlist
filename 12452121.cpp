b8ef00d9a616a8b1a6940058484a747dd34b2099
paw2kk <wndxakk@gmail.com>
2025-05-09 08:28:43 +0700

    menyelesaikan kasus 4

--- 12452121.cpp
+++ 12452121.cpp
@@ -161,7 +161,15 @@ int main()
                 cout << endl
                 << "Masukkan no mahasiswa yang dicari : ";
                 cin >> nim;
-                if
+                if (mhs.Search(nim, &previous, &current) == false)
+                cout << endl
+                << "Data tidak ditemukan" << endl;
+                else{
+                    cout << endl
+                    << "Data ditemukan" << endl;
+                    cout << "\nNo Mahasiswa: " << current->noMhs << endl;
+                    cout << "\n";
+                }
             }
         }
     }
