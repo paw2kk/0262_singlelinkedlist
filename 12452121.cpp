3305d27e9e7c63dc73a0c6e670d0089c2e9f0014
paw2kk <wndxakk@gmail.com>
2025-05-09 08:04:56 +0700

    membuat class linkedlist

--- 12452121.cpp
+++ 12452121.cpp
@@ -9,3 +9,48 @@ class Node
     Node *next;
 };
 
+class LinkedList
+{
+    Node *START;
+
+    public:
+    LinkedList()
+    {
+        START = NULL;
+    }
+
+    void addNode(){
+        int nim;
+        cout << "\nMasukkan Nomor Mahasiswa: ";
+        cin >> nim;
+
+        Node *nodeBaru = new Node;
+        nodeBaru->noMhs = nim;
+
+        if (START == NULL || nim <= START->noMhs)
+        {
+            if ((START != NULL ) && (nim >= current->noMhs))
+            {
+                cout << "\nDuplikasi no Mhs tidak diijinkan\n";
+                return;
+            }
+            nodeBaru->next = START;
+            START = nodeBaru;
+            return;
+        }
+        Node *previous = START;
+        Node *current = START;
+        while ((current != NULL) && (nim >= current->noMhs))
+        {
+            if (nim == current->noMhs)
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+            previous = current;
+            current = current->next;
+        }
+        nodeBaru->next = current;
+        previous->next = nodeBaru;
+    }
+};
